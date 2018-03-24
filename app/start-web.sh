7638b404bbb22eedb076031be6d1607803743c20
Matthew Segal <mattdsegal@gmail.com>
2018-03-24 14:55:50 +1100

    Add gunicorn

--- app/start-web.sh
+++ app/start-web.sh
@@ -1,2 +1,20 @@
 #!/bin/bash
-./manage.py runserver 0.0.0.0:8000
+echo "Starting reddit app as `whoami`"
+
+echo "Running migrations"
+./manage.py migrate
+
+# echo "Collecting static files."
+# ./manage.py collectstatic --noinput
+
+mkdir -p /var/log/gunicorn
+
+echo "Starting gunicorn"
+gunicorn reddit.wsgi:application \
+  --name reddit \
+  --workers 3 \
+  --bind 0.0.0.0:8000 \
+  --capture-output \
+  --log-level info \
+  --error-logfile /var/log/gunicorn/access.log \
+  --access-logfile /var/log/gunicorn/error.log
