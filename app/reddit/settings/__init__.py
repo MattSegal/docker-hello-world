3be8211ebdbe8438f96a30e7dcc63978f6acae8e
Matthew Segal <mattdsegal@gmail.com>
2018-04-01 20:54:42 +1000

    improve docker config

--- app/reddit/settings/__init__.py
+++ app/reddit/settings/__init__.py
@@ -66,11 +66,11 @@ WSGI_APPLICATION = 'reddit.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT'),
+        'NAME': os.environ.get('REDDIT_DB_NAME'),
+        'USER': os.environ.get('REDDIT_DB_USER'),
+        'PASSWORD': os.environ.get('REDDIT_DB_PASSWORD'),
+        'HOST': os.environ.get('REDDIT_DB_HOST'),
+        'PORT': os.environ.get('REDDIT_DB_PORT'),
     }
 }
 
