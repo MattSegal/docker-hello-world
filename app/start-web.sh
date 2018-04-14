a4589192e97d0d4daaf5919d09e146b72500e152
Matthew Segal <mattdsegal@gmail.com>
2018-04-14 14:49:45 +1000

    debug web container

--- app/start-web.sh
+++ app/start-web.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-echo "Starting reddit app as `whoami`" && \
-echo "Running migrations" && \
-./manage.py migrate && \
-mkdir -p /var/log/gunicorn && \
-echo "Starting gunicorn" && \
-gunicorn reddit.wsgi:application \
-  --name reddit \
-  --workers 3 \
-  --bind 0.0.0.0:8003 \
-  --capture-output \
-  --log-level info \
-  --error-logfile /var/log/gunicorn/error.log \
-  --access-logfile /var/log/gunicorn/access.log
+sleep 3h
+# echo "Starting reddit app as `whoami`" && \
+# echo "Running migrations" && \
+# ./manage.py migrate && \
+# mkdir -p /var/log/gunicorn && \
+# echo "Starting gunicorn" && \
+# gunicorn reddit.wsgi:application \
+#   --name reddit \
+#   --workers 3 \
+#   --bind 0.0.0.0:8002 \
+#   --capture-output \
+#   --log-level info \
+#   --error-logfile /var/log/gunicorn/error.log \
+#   --access-logfile /var/log/gunicorn/access.log
