b5af61ad056c54b44ff4c46b771e4d550a1addfd
Matthew Segal <mattdsegal@gmail.com>
2018-04-14 14:57:06 +1000

    fixup gunicorn after debugging

--- app/start-web.sh
+++ app/start-web.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
-sleep 3h
-# echo "Starting reddit app as `whoami`" && \
-# echo "Running migrations" && \
-# ./manage.py migrate && \
-# mkdir -p /var/log/gunicorn && \
-# echo "Starting gunicorn" && \
-# gunicorn reddit.wsgi:application \
-#   --name reddit \
-#   --workers 3 \
-#   --bind 0.0.0.0:8002 \
-#   --capture-output \
-#   --log-level info \
-#   --error-logfile /var/log/gunicorn/error.log \
-#   --access-logfile /var/log/gunicorn/access.log
+echo "Starting reddit app as `whoami`" && \
+echo "Running migrations" && \
+./manage.py migrate && \
+mkdir -p /var/log/gunicorn && \
+echo "Starting gunicorn" && \
+gunicorn reddit.wsgi:application \
+  --name reddit \
+  --workers 3 \
+  --bind 0.0.0.0:8002 \
+  --capture-output \
+  --log-level info \
+  --error-logfile /var/log/gunicorn/error.log \
+  --access-logfile /var/log/gunicorn/access.log
